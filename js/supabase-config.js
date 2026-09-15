481a13f7349c17440b499869ffa62b800b02a292
newcpppgimo-boop <newcpppgimo@gmail.com>
2026-09-15 21:08:46 +0800

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://adfusiumugdgbaieawqsw.supabase.co";
+const SUPABASE_URL = "https://adfusiumugdbaieawqsw.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_dIjQKoLPJp27b_fCgQotbg_-YGmz-i9";
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
